3b1e44b5543244cb650248bba21a684f0d3f8a9a
a <a>
2020-11-10 13:01:56 +0900

    メイン作成

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -35,4 +35,18 @@ int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
 	return Num;
 }
 
-
+int main() {
+	std::cout << nth_prime(367, 186, 151) << std::endl;
+	std::cout << nth_prime(179, 10, 203) << std::endl;
+	std::cout << nth_prime(271, 37, 39) << std::endl;
+	std::cout << nth_prime(103, 230, 1) << std::endl;
+	std::cout << nth_prime(27, 104, 185) << std::endl;
+	std::cout << nth_prime(253, 50, 85) << std::endl;
+	std::cout << nth_prime(1, 1, 1) << std::endl;
+	std::cout << nth_prime(9075, 337, 210) << std::endl;
+	std::cout << nth_prime(307, 24, 79) << std::endl;
+	std::cout << nth_prime(331, 221, 177) << std::endl;
+	std::cout << nth_prime(259, 170, 40) << std::endl;
+	std::cout << nth_prime(269, 58, 102) << std::endl;
+	return 0;
+}
